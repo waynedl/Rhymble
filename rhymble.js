b166125b0d85ccd580b63666c84b7e4b3ada8a79
Richard Telep <rick.telep@gmail.com>
2011-06-06 10:39:12 -0400

    using iscroll 4

--- rhymble.js
+++ rhymble.js
@@ -15,6 +15,13 @@ function Round(){
     R.buffer = new Array();  // R.buffer_length most recent guesses
     R.buffer_length = 5;
     R.buffer_index = 0;
+    
+    // iScroll dummies
+    R.slots_scroll = {
+	'1':	{refresh: function(){}},	
+	'2':	{refresh: function(){}},	
+	'3':	{refresh: function(){}}	
+    };
 
     R.load_success = function(data){
 	n = get_random_in_range(0, data.length);
@@ -31,9 +38,19 @@ function Round(){
 	});
     };
 
+    R.draw_slots = function(n_syllables, rhymes){
+	var slots_ul = $('#slots'+n_syllables+' UL');
+	$.each(rhymes, function(k, v){
+	    slots_ul.append('<li class="slot" word="'+v+'" syllables="'+n_syllables+'" index="'+k+'"><span class="hidden_slot">'+v+'</span></li>');
+	});
+	R.slots_scroll[n_syllables] = new iScroll('slots'+n_syllables);
+    }
 
     R.draw_round = function(){
-
+	$('#prompt').html(R.round.prompt);
+	if(R.round.rhymes[1]){R.draw_slots(1,R.round.rhymes[1]);}
+	if(R.round.rhymes[2]){R.draw_slots(2,R.round.rhymes[2]);}
+	if(R.round.rhymes[3]){R.draw_slots(3,R.round.rhymes[3]);}
     };
 
     R.to_buffer = function(v){
@@ -46,11 +63,18 @@ function Round(){
         R.buffer_index = 0;
     };
 
-
-    $('#input FORM').submit(function(){
-        // Do not ever submit this form.  It's only form to get tabindex.
-        return false
-    });
+    R.resize = function(){
+	// Round area
+	var round_height = $(window).height() - 200; 
+	$('#round').height(round_height);
+	
+	// Slots and Scrolling
+	var slots_height = round_height - $('#text_area').height();
+	$('#slots').height(slots_height);
+	R.slots_scroll[1].refresh();
+	R.slots_scroll[2].refresh();
+        R.slots_scroll[3].refresh();
+    };
 
     $('body').bind('finishedLoadingRound',R.draw_round);
 
@@ -70,8 +94,10 @@ function Game(){
     var G = this;
     G.rounds = new Array();
     G.nextRound = 0;
+    G.this_round = null; // current Round object.
     G.n_rounds = 10;
-    G.time = 1 * 60;  // 1 minutes of seconds 
+    G.time = 5;  //seconds 
+    G.current_guess = new Array();
 
     G.resize = function(){
 	// Keyboard Buttons
@@ -79,33 +105,49 @@ function Game(){
 	$('.key').css({width: w+'px'});
 	$('#row2, #row3').css({paddingLeft: w/2});
 
-	// Round area
-	var round_height = $(window).height() - 200; 
-	$('#round').height(round_height);
-
+    	// Let the round resize itself.
+	if (G.this_round){G.this_round.resize();}
     }
+
+    G.submit_guess = function(guess){
+	alert(guess);
+    };
 
     G.draw_keyboard = function(){
 	var keyboard = $('#keyboard');
 	function make_row_of_buttons(n_row, row){
 	    var div = $('#row'+n_row);
-	    if (n_row==3){div.append('<img class="key" id="go_button" src="images/go.png" />');}
+	    if (n_row==3){div.append('<div class="key" letter="go"><img id="go_button" src="images/go.png" />');}
             $.each(row, function(k,v){
 		div.append('<div class="key" letter="'+v+'"><span>'+v+'</span></div>');    
 	    });
-	    if (n_row==3){div.append('<img class="key" id="del_button" src="images/del.png" />');}
+	    if (n_row==3){div.append('<div class="key" letter="del"><img id="del_button" src="images/del.png" />');}
 	    keyboard.append(div);
 	}
 	make_row_of_buttons(1, 'qwertyuiop');
 	make_row_of_buttons(2, 'asdfghjkl');
 	make_row_of_buttons(3, 'zxcvbnm');
 	
-	
 	$('.key').bind('tap', function(e){
 	    e.stopPropagation();
 	    e.stopImmediatePropagation();
+    
+	    // Typing in #text_area
 	    var letter = $(this).attr('letter');
-	    $('#text_area').append('<span>'+letter+'</span>');
+	    if (letter=='del'){
+		G.current_guess.pop();
+		$('#text_area').html(G.current_guess.join(''));
+	    } else if (letter=='go'){
+		var word = $();
+		G.submit_guess(G.current_guess.join(''));
+		G.current_guess = new Array();
+		$('#text_area').html(G.current_guess.join(''));
+	    } else {
+	    	G.current_guess.push(letter);
+		$('#text_area').html(G.current_guess.join(''));
+	    }
+
+	    // Animate tap
 	    var tap_mask = $('<span class="mask">&nbsp;</span>'); 
 	    $(this).append(tap_mask);
 	    tap_mask.animate({opacity: 0}, function(){$(this).remove();})
@@ -120,24 +162,21 @@ function Game(){
             G.gameOver();
         }
         else {
-            G.rounds.push(new Round());
-            G.nextRound += 1;
+	    G.this_round = new Round();
+            G.rounds.push(G.this_round);
         }
     };
     
     G.gameOver = function(){
         clearInterval(G.intervalID);
-        $('INPUT[name=give-up]').trigger('click');
-        $('INPUT').unbind();
-        $('INPUT').blur();
-        $('body').append('<div id="cover"></div>');
-        $('body').append('<h1 id="game-over">score: <span id="final-score">'+score+'</span></h1>');
-        $('#cover').show(600);        
+        $('body').append('<div id="round-over-mask"></div>');
+        $('body').append('<h1 id="round-over">score: <span id="final-score">'+score+'</span></h1>');
+        $('#round-over-mask').show(600);        
     };
 
     G.timer = function(){
         G.intervalID = setInterval('decrementTimer()', 1000);
-
+	$('#clock').show();
         decrementTimer = function (){
             $('#minutes').html((G.time/60).toString().split('.')[0]);
             var seconds = (G.time % 60).toString();
@@ -152,6 +191,7 @@ function Game(){
     G.draw_keyboard();
     G.resize();
     G.getNextRound();
+    G.timer();	
     
     return true
 };
